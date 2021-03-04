7d1163dc009db7609706ca9175a1105207a49f3c
syifa Rizka Sagita <syifarizka23@gmail.com>
2021-03-04 12:43:35 +0700

    tambah model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MataKuliah, Tugas
 
 # Register your models here.
+admin.site.register(MataKuliah)
+admin.site.register(Tugas)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class MataKuliah(models.Model):  
+    nama = models.CharField(max_length=250)
+    dosen = models.CharField(max_length=250)
+    sks = models.IntegerField()
+    deskripsi = models.TextField()
+    semester = models.CharField(max_length=6)
+    tahun_ajar = models.CharField(max_length=250)
 
+class Tugas(models.Model):
+    name = models.CharField(max_length=125, default='none')
+    deadline = models.DateField()
+    mataKuliah = models.ForeignKey(MataKuliah, on_delete=models.CASCADE, default='none', null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
